786e92ac01202704e60b8b1c6354e1e69d4896e7
Raresh Nistor <raresh@nistor.email>
2021-04-30 23:57:41 +1200

    do something

--- SuperSize.Plugin/Config/Primitive.cs
+++ SuperSize.Plugin/Config/Primitive.cs
@@ -13,7 +13,10 @@ namespace SuperSize.Plugin.Config
     public class Primitive : IElement
     {
 
-        public static readonly ImmutableArray<Type> AcceptedTypes = ImmutableArray.Create(new Type[]
+        /// <summary>
+        /// Accepted primitive types.
+        /// </summary>
+        public static ImmutableArray<Type> AcceptedTypes { get; } = ImmutableArray.Create(new Type[]
         {
             typeof(string),
             typeof(int),
@@ -21,8 +24,15 @@ namespace SuperSize.Plugin.Config
             typeof(bool),
         });
 
+        /// <summary>
+        /// Primitive value.
+        /// </summary>
         public object? Value { get; }
 
+        /// <summary>
+        /// Creates a primitive value.
+        /// </summary>
+        /// <param name="value">Value to encapsulate.</param>
         public Primitive(object? value)
         {
             // save if null
@@ -37,43 +47,89 @@ namespace SuperSize.Plugin.Config
             Value = value;
         }
 
+        /// <summary>
+        /// Creates a primitive string.
+        /// </summary>
+        /// <param name="value">String to encapsulate.</param>
         public Primitive(string value)
         {
             Value = value;
         }
 
+        /// <summary>
+        /// Creates a primitive integer.
+        /// </summary>
+        /// <param name="value">Integer to encapsulate.</param>
         public Primitive(int value)
         {
             Value = value;
         }
 
+        /// <summary>
+        /// Creates a primitive number.
+        /// </summary>
+        /// <param name="value">Number to encapsulate.</param>
         public Primitive(float value)
         {
             Value = value;
         }
 
+        /// <summary>
+        /// Creates a primitive boolean.
+        /// </summary>
+        /// <param name="value">Boolean to encapsulate.</param>
         public Primitive(bool value)
         {
             Value = value;
         }
 
+        /// <summary>
+        /// Creates a null primitive.
+        /// </summary>
         public Primitive()
         {
             Value = null;
         }
 
+        /// <summary>
+        /// Is this primitive null?
+        /// </summary>
         public bool IsNull => Value == null;
 
+        /// <summary>
+        /// Get this value as cast it as a type.
+        /// </summary>
+        /// <typeparam name="T">Target type.</typeparam>
+        /// <returns>Value as <typeparamref name="T"/></returns>
         public T Get<T>() => Value != null ? (T)Value : FailNullCast<T>();
 
+        /// <summary>
+        /// Get this value as a string.
+        /// </summary>
+        /// <returns>Value as string.</returns>
         public string GetString() => Get<string>();
 
+        /// <summary>
+        /// Get this value as an integer.
+        /// </summary>
+        /// <returns>Value as integer.</returns>
         public int GetInt32() => Get<int>();
 
+        /// <summary>
+        /// Get this value as a number.
+        /// </summary>
+        /// <returns>Value as floating-point number.</returns>
         public float GetSingle() => Get<float>();
 
+        /// <summary>
+        /// Get this value as a boolean.
+        /// </summary>
+        /// <returns>Value as boolean.</returns>
         public bool GetBoolean() => Get<bool>();
 
+        /// <summary>
+        /// Throw a null exception.
+        /// </summary>
         private static T FailNullCast<T>()
             => throw new InvalidCastException($"Couldn't cast primitive as {typeof(T)}. Is null.");
     }
---
+++ SuperSize.Plugin/PluginBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SuperSize.Plugin
+{
+    /// <summary>
+    /// Represents the plugin. When the plugin is installed and initialised,
+    /// SuperSize looks for the first class that inherits this class.
+    /// <br/>
+    /// It's recommended to have only one Plugin class in your assembly.
+    /// </summary>
+    public abstract class PluginBase
+    {
+        /// <summary>
+        /// Plugin's display name.
+        /// </summary>
+        public abstract string Name { get; }
+
+        /// <summary>
+        /// Plugin's author.
+        /// </summary>
+        public abstract string Author { get; }
+
+        /// <summary>
+        /// Plugin's available logic.
+        /// </summary>
+        public virtual IEnumerable<Logic> AvailableLogic { get; } = new List<Logic>();
+
+        /// <summary>
+        /// Automatically discover the logic in an assembly.
+        /// </summary>
+        /// <param name="assembly">Assembly to discover logic in.</param>
+        /// <returns></returns>
+        public static IEnumerable<Logic> DiscoverLogic(Assembly assembly)
+        {
+            // TODO: make big brain code to automatically discover logic
+            // extending the Logic class with a constructor taking 0 arguments.
+            throw new NotImplementedException();
+        }
+    }
+}
