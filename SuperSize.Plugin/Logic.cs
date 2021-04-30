792cf06d596767c3faf241bb976a95864ab83436
Raresh Nistor <raresh@nistor.email>
2021-04-30 21:53:44 +1200

    make the plugin base be a winforms class library

--- SuperSize.Plugin/Logic.cs
+++ SuperSize.Plugin/Logic.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SuperSize.Plugin
 {
-    public abstract class SizingLogic
+    public abstract class Logic
     {
         /// <summary>
         /// The name of the logic available seen by the user.
