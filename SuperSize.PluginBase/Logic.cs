fb1b3464c9b749941f3ae416afac1a68aae00893
Raresh Nistor <raresh@nistor.email>
2022-06-16 16:01:04 +1200

    Code cleanup

--- SuperSize.PluginBase/Logic.cs
+++ SuperSize.PluginBase/Logic.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SuperSize.PluginBase
 {
-
     public abstract class Logic
     {
         /// <summary>
