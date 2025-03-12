6ac1ee2659996659002a783535994258b62c72b3
Raresh Nistor <raresh@nistor.email>
2025-03-13 10:53:40 +1300

    remove installer and update CI dotnet ver

--- BuildScripts/Utilities.ps1
+++ BuildScripts/Utilities.ps1
@@ -1,15 +1,15 @@
 $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
 
-$targetPlatforms = @(
-    "win-x86"
-    "win-x64"
-    # "win-arm"
-    # "win-arm64"
-)
+function Get-TargetPlatforms () {
+    @(
+        "win-x64"
+        "win-arm64"
+    )
+}
 
 function Get-ProjectVersion () {
     Select-Xml -Path SuperSize/SuperSize.csproj -XPath '/Project/PropertyGroup/Version' |
-        ForEach-Object {
-            $_.Node.InnerXML
-        }
+    ForEach-Object {
+        $_.Node.InnerXML
+    }
 }
