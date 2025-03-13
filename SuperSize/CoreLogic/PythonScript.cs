639fb69e84cee0209968480277650c6b3c609d9f
Raresh Nistor <raresh@nistor.email>
2025-03-13 22:39:16 +1300

    migrate python logic to use the new primitives

--- SuperSize/CoreLogic/PythonScript.cs
+++ SuperSize/CoreLogic/PythonScript.cs
@@ -1,6 +1,7 @@
 ﻿using IronPython.Hosting;
 using Microsoft.Scripting.Hosting;
 using SuperSize.Model;
+using SuperSize.Scripting.Python;
 using SuperSize.UI.Dialogs;
 using System;
 using System.Collections.Generic;
@@ -17,53 +18,35 @@ internal sealed class PythonScript : Logic
 {
     public override string DisplayName => "Scripting: Python";
 
-    private ScriptRuntime _scriptRuntime;
-    private ScriptScope _scriptScope;
-    private ScriptEngine _scriptEngine;
+    public override Task<Rectangle> CalculateWindowSize(Settings settings) => Task.Run(() =>
+        {
+            if (!settings.TryGetValue("Script", out var script))
+            {
+                MessageBox.Show(
+                    "There is no script loaded. Please create or import a script.",
+                    "SuperSize - Missing Python Script", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw new Exception("TODO: No script loaded. Better exception pls");
+            }
 
-    public PythonScript()
-    {
-        _scriptRuntime = Python.CreateRuntime();
-        _scriptScope = _scriptRuntime.CreateScope();
-        _scriptEngine = _scriptRuntime.GetEngine("Python");
-        PythonSetup();
-    }
+            var context = new PythonContext();
+            context.Execute(script!);
 
-    public override Task<Rectangle> CalculateWindowSize(Settings settings)
-    {
-        return Task.FromResult(new Rectangle(0, 0, 0, 0));
-    }
+            if (context.Result is Rectangle result)
+            {
+                return result;
+
+            }
+
+            MessageBox.Show(
+                "The script did not yield a result. Please check the script.",
+                "SuperSize - No Yield",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            throw new Exception("TODO: Script didn't yield(). Better exception pls");
+        });
 
     public override void ShowSettings(Settings settings)
     {
         new PythonScriptEditor().ShowDialog();
-    }
-
-    private void PythonSetup()
-    {
-        _scriptRuntime.LoadAssembly(Assembly.GetExecutingAssembly());
-        _scriptRuntime.LoadAssembly(Assembly.Load("System.Drawing"));
-        _scriptRuntime.LoadAssembly(Assembly.Load("System.Windows.Forms"));
-
-        PopulatePythonScope(new()
-        {
-            { "alert", new Action<object>(message => MessageBox.Show(message.ToString())) },
-
-            // screen detail
-            { "get_screens", new Func<Screen[]>(() => Screen.AllScreens) },
-            { "get_all_screen_bounds", new Func<Rectangle>(() => OS.Utilities.GetAllScreenBounds()) },
-
-            // python shorthands .NET object constructors
-            { "rectangle", new Func<int, int, int, int, Rectangle>((x, y, w, h) => new Rectangle(x, y, w, h)) },
-            { "point", new Func<int, int, Point>((x, y) => new Point(x, y)) },
-        });
-    }
-
-    private void PopulatePythonScope(Dictionary<string, object?> values)
-    {
-        foreach (var (key, value) in values)
-        {
-            _scriptScope.SetVariable(key, value);
-        }
     }
 }
