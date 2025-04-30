c21cfd291c2bc7428a0e400ff34c94abef68f4ac
Rareș Nistor <raresh@nistor.email>
2025-04-30 16:27:18 +1200

    Add a "Start with Windows" checkbox on the settings page (#21)
    * add a "Start with Windows" checkbox on the settings page
    * some additional cleanup
    * add some safety around the registry edits

---
+++ SuperSize/OS/SystemStartup.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+
+namespace SuperSize.OS;
+
+/// <summary>
+/// Helpers utilities to help with starting the application when Windows boots in.
+/// </summary>
+public static class SystemStartup
+{
+    private static string RegistryValueName => "SuperSize";
+
+    private static string ExecutablePath => Environment.ProcessPath!;
+
+    private static RegistryKey? OpenSubKey(bool writable)
+    {
+        var baseKey = Registry.CurrentUser;
+        if (baseKey is null)
+        {
+            Debug.Print("There is no current user registry base key");
+            return null;
+        }
+
+        return baseKey?.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", writable);
+    }
+
+    /// <summary>
+    /// Register the application for startup.
+    /// </summary>
+    public static void Register()
+    {
+        using var key = OpenSubKey(true) ?? throw new Exception("Startup applications registry key doesn't exist.");
+        try
+        {
+            key.SetValue(RegistryValueName, ExecutablePath);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine("Exception while registering: {0}", ex);
+            throw new Exception("Couldn't register to start with Windows", ex);
+        }
+    }
+
+    /// <summary>
+    /// Unregister the applicatiom from startup.
+    /// </summary>
+    public static void Unregister()
+    {
+        using var key = OpenSubKey(true) ?? throw new Exception("Startup applications registry key doesn't exist.");
+        try
+        {
+            key.DeleteValue(RegistryValueName);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine("Exception while unregistering: {0}", ex);
+            throw new Exception("Couldn't deregister from starting with Windows.", ex);
+        }
+    }
+
+    /// <summary>
+    /// Check if the application registration is current.
+    /// </summary>
+    public static bool IsRegistered()
+    {
+        using var key = OpenSubKey(false);
+        if (key is null) return false;
+
+        return key.GetValue(RegistryValueName, "").ToString() == ExecutablePath;
+    }
+}
--- SuperSize/UI/Controls/SettingsPage.cs
+++ SuperSize/UI/Controls/SettingsPage.cs
@@ -1,4 +1,5 @@
 ﻿using SuperSize.Model;
+using SuperSize.OS;
 using SuperSize.Service;
 using SuperSize.UI.Dialogs;
 using SuperSize.UI.Forms;
@@ -30,6 +31,8 @@ namespace SuperSize.UI.Controls
             // update components
             UpdateSizePreview();
             UpdateKeybindPreview();
+
+            _systemStartupCheckbox.Checked = SystemStartup.IsRegistered();
         }
 
         private void UpdateSizePreview()
@@ -152,7 +155,8 @@ namespace SuperSize.UI.Controls
             var logic = SizeService.SelectedLogic;
             var result = logic?.Calculate();
 
-            if (result == null) {
+            if (result == null)
+            {
                 MessageBox.Show("The logic didn't return a result.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
@@ -167,6 +171,27 @@ namespace SuperSize.UI.Controls
         private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
         {
 
+        }
+
+        private void OnSystemStartupChanged(object sender, EventArgs e)
+        {
+            if (sender is not CheckBox checkBox) return;
+
+            try
+            {
+                if (checkBox.Checked)
+                {
+                    SystemStartup.Register();
+                }
+                else
+                {
+                    SystemStartup.Unregister();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "SuperSize", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
